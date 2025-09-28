c963f5a387969e4ef5c1820669037942d45431f6
1 K <a12345852021022021@163.com>
2025-09-29 00:09:25 +0800

    feat(redis): 引入Lua脚本优化分布式锁解锁逻辑- 添加了DefaultRedisScript用于加载Lua脚本 - 实现了通过execute方法调用Lua脚本来安全删除锁 - 创建了slock.lua文件以原子化方式验证并删除锁- 避免了Java端多次网络请求带来的竞态条件- 提升了解锁操作的性能与可靠性

--- src/main/java/com/hmdp/utils/SimpleRedisLock.java
+++ src/main/java/com/hmdp/utils/SimpleRedisLock.java
@@ -1,7 +1,11 @@
 package com.hmdp.utils;
 
 import cn.hutool.core.lang.UUID;
+import org.springframework.core.io.ClassPathResource;
 import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.data.redis.core.script.DefaultRedisScript;
+
+import java.util.List;
 
 import static java.util.concurrent.TimeUnit.SECONDS;
 
@@ -9,7 +13,13 @@ public record SimpleRedisLock(String name, StringRedisTemplate sRedis) implement
 
     private static final String KEY_PREFIX = "lock:";
     private static final String ID_PREFIX = UUID.randomUUID().toString(true) + "-";
+    private static final DefaultRedisScript<Long> UNLOCK_SCRIPT;
 
+    static {
+        UNLOCK_SCRIPT = new DefaultRedisScript<>();
+        UNLOCK_SCRIPT.setLocation(new ClassPathResource("script/slock.lua"));
+        UNLOCK_SCRIPT.setResultType(Long.class);
+    }
 
     @Override
     public boolean tryLock(long timeoutSec) {
@@ -20,11 +30,9 @@ public record SimpleRedisLock(String name, StringRedisTemplate sRedis) implement
 
     @Override
     public void unlock() {
-        var key = KEY_PREFIX + name;
-        var sv = sRedis.opsForValue().get(key);
-        var threadId = ID_PREFIX + Thread.currentThread().getId();
-        if (sv != null && sv.equals(threadId)) {
-            sRedis.delete(key);
-        }
+        sRedis.execute(UNLOCK_SCRIPT, List.of(KEY_PREFIX + name), ID_PREFIX + Thread.currentThread().getId());
     }
+
 }
+
+

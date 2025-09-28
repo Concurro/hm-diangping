743a289ec1e710d8483121dfea4833803c866d0b
1 K <a12345852021022021@163.com>
2025-09-28 20:44:03 +0800

    refactor(utils):优化Redis锁实现- 将锁键前缀重命名为KEY_PREFIX- 添加ID_PREFIX用于标识线程ID- 修改tryLock方法使用线程ID替代线程名称 - 实现unlock方法的安全删除逻辑- 引入hutool的UUID工具生成唯一标识符 - 增强锁释放时的线程匹配验证

--- src/main/java/com/hmdp/utils/SimpleRedisLock.java
+++ src/main/java/com/hmdp/utils/SimpleRedisLock.java
@@ -1,22 +1,30 @@
 package com.hmdp.utils;
 
+import cn.hutool.core.lang.UUID;
 import org.springframework.data.redis.core.StringRedisTemplate;
 
 import static java.util.concurrent.TimeUnit.SECONDS;
 
 public record SimpleRedisLock(String name, StringRedisTemplate sRedis) implements ILock {
 
-    private static final String key_prefix = "lock:";
+    private static final String KEY_PREFIX = "lock:";
+    private static final String ID_PREFIX = UUID.randomUUID().toString(true) + "-";
+
 
     @Override
     public boolean tryLock(long timeoutSec) {
-        var key = key_prefix + name;
-        var threadName = Thread.currentThread().getName();
-        return Boolean.TRUE.equals(sRedis.opsForValue().setIfAbsent(key, threadName, timeoutSec, SECONDS));
+        var key = KEY_PREFIX + name;
+        var threadId = ID_PREFIX + Thread.currentThread().getId();
+        return Boolean.TRUE.equals(sRedis.opsForValue().setIfAbsent(key, threadId, timeoutSec, SECONDS));
     }
 
     @Override
     public void unlock() {
-        sRedis.delete(key_prefix + name);
+        var key = KEY_PREFIX + name;
+        var sv = sRedis.opsForValue().get(key);
+        var threadId = ID_PREFIX + Thread.currentThread().getId();
+        if (sv != null && sv.equals(threadId)) {
+            sRedis.delete(key);
+        }
     }
 }
